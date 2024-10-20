d2e72d41a19fb2c3d4e702d5bb9b3f5ff76f5da2
Vanilla M <bidisaaaa@gmail.com>
2024-10-21 03:25:53 +0530

    Added userinfo command and fixed the embed

--- index.js
+++ index.js
@@ -74,6 +74,31 @@ client.on('messageCreate', async message => {
       message.channel.send('You need to mention a member!');
     }
   }
+
+    // Userinfo
+    if (message.content.startsWith('!userinfo')) {
+        const member = message.mentions.members.first() || message.member;
+    
+        const roles = member.roles.cache
+          .filter(role => role.id !== message.guild.id)
+          .map(role => `<@&${role.id}>`)
+          .join(', ') || 'No roles';
+    
+        const userInfoEmbed = new EmbedBuilder()
+          .setColor('#000000')
+          .setTitle(`User Info for ${member.user.tag}`)
+          .setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 1024 }))
+          .addFields(
+            { name: 'User ID', value: member.user.id, inline: true },
+            { name: 'Account Created', value: member.user.createdAt.toDateString(), inline: true },
+            { name: 'Joined Server', value: member.joinedAt.toDateString(), inline: true },
+            { name: 'Nickname', value: member.displayName, inline: true },
+            { name: 'Roles', value: roles, inline: false }
+          )
+          .setTimestamp();
+    
+        message.channel.send({ embeds: [userInfoEmbed] });
+      }
 });
 
 client.login(process.env.DISCORD_TOKEN);
